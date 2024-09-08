68a224d98ac323ac5a787263d66e4215fd420aac
Bunyamin Genc <bunyaming@uia.no>
2024-09-08 10:20:15 +0200

    @apply methods

--- movie-app-starter/src/App.js
+++ movie-app-starter/src/App.js
@@ -10,15 +10,22 @@ const App = () => {
         Hello world!
       </h1>
 
-     <div className=' flex gap-3 mt-4'>
+     <div className=' flex flex-col sm:flex-row  gap-3 mt-4'>
      <div className='flex flex-col justify-between items-center bg-slate-500 rounded-xl w-3/12 h-[250px]'>
+
+      
+     <img src="https://www.themoviedb.org/t/p/w1280/37p95Lpe7mfVX86ZcVr7TisAHPn.jpg" alt="" className="rounded-xl h-[200px] animate-pulse"    />
+
+     <p className='line-clamp-1 hover:line-clamp-none'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestias eaque expedita laborum officiis exercitationem repudiandae id dolor.</p>
+      
+     
        </div>
        <div className= "card">
 
-        <img src="https://www.themoviedb.org/t/p/w1280/37p95Lpe7mfVX86ZcVr7TisAHPn.jpg" alt="" className="rounded-xl h-[250px] animate-pulse"   />
+        <img src="https://www.themoviedb.org/t/p/w1280/37p95Lpe7mfVX86ZcVr7TisAHPn.jpg" alt="" className="rounded-xl h-[200px] animate-pulse"   />
         <p className='line-clamp-1 hover:line-clamp-none'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestias eaque expedita laborum officiis exercitationem repudiandae id dolor.</p>
        </div>
-    {/*  //* @layer og @aply ozellikleri */}
+
      </div>
        
     </div>
